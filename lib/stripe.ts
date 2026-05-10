aef7da5c600b3036687e37e3f657b17da25e9ec9
Akitzuu <lukasduprat.trivec@gmail.com>
2026-05-11 01:57:40 +0200

    fix: remove Stripe apiVersion to use default

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,7 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2026-04-22.dahlia" as any,
-});
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY! as any);
 
 export const PLANS = {
   STARTER: {
